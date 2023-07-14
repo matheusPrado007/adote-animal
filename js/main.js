2eeaa160edfd40143ca0ccec6fb6e8ce175af25c
Prado <peu_prado01@hotmail.com>
2023-07-14 20:25:16 -0300

    feat: ajusta rota login e validações

--- js/main.js
+++ js/main.js
@@ -1,41 +1,92 @@
-const login = document.querySelector(".login-form");
-const loginBtn = document.querySelector("#login-btn");
-const navbar = document.querySelector(".header .navbar");
-const menuBtn = document.querySelector("#menu-btn");
+document.addEventListener("DOMContentLoaded", function() {
+  const login = document.querySelector(".login-form");
+  const loginBtn = document.getElementById("login-btn");
+  const navbar = document.querySelector(".header .navbar");
+  const menuBtn = document.getElementById("menu-btn");
+  const btnUser = document.getElementById('user');
 
-loginBtn.onclick = () => {
-  login.classList.toggle('active');
-  navbar.classList.remove('active');
-};
+  loginBtn.addEventListener('click', () => {
+    login.classList.toggle('active');
+    navbar.classList.remove('active');
+  });
 
-menuBtn.onclick = () => {
-  login.classList.remove('active');
-  navbar.classList.toggle('active');
-};
+  menuBtn.addEventListener('click', () => {
+    login.classList.remove('active');
+    navbar.classList.toggle('active');
+  });
 
-window.onscroll = () => {
-  login.classList.remove('active');
-  navbar.classList.remove('active');
-};
+  window.addEventListener('scroll', () => {
+    login.classList.remove('active');
+    navbar.classList.remove('active');
+  });
 
-const swiper = new Swiper(".gallery-slider", {
-  grabCursor: true,
-  loop: true,
-  centeredSlides: true,
-  spaceBetween: 20,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  breakpoints: {
-    0: {
-      slidesPerView: 1,
+  const swiper = new Swiper(".gallery-slider", {
+    grabCursor: true,
+    loop: true,
+    centeredSlides: true,
+    spaceBetween: 20,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
     },
-    700: {
-      slidesPerView: 2,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      700: {
+        slidesPerView: 2,
+      },
     },
-  },
+  });
+
+  const checkboxStorage = document.getElementById('storage');
+  const emailUser = document.getElementById('email');
+  const password = document.getElementById('password');
+
+  const dataUser = [
+    {
+      email: 'matheuspradodeveloper@gmail.com',
+      senha: 'Pitchichito'
+    },
+    {
+      email: 'matheuspradodeveloper@gmail.com',
+      senha: 'Amerelão'
+    }
+  ];
+
+  function saveToLocalStorage() {
+    if (checkboxStorage.checked) {
+      localStorage.setItem('email', emailUser.value);
+      localStorage.setItem('senha', password.value);
+    } else {
+      localStorage.removeItem('email');
+      localStorage.removeItem('senha');
+      console.log('Dados removidos do localStorage');
+    }
+  }
+
+  function verification(event) {
+    event.preventDefault();
+    const emailValue = emailUser.value;
+    const senhaValue = password.value;
+
+    const regex = /^[\w.-]+@[a-zA-Z_-]+?(?:\.[a-zA-Z]{2,})+$/;
+    const verifyUser = dataUser.find(user => user.email.includes(emailValue) && user.senha.includes(senhaValue));
+
+    if (!regex.test(emailValue) || !verifyUser) {
+      alert('Email ou senha inválidos.');
+    } else {
+      window.location.href = '/pages/user/user.html';
+      saveToLocalStorage();
+    }
+  }
+
+  btnUser.addEventListener('click', verification);
+
+  const savedEmail = localStorage.getItem('email');
+  const savedPassword = localStorage.getItem('senha');
+  if (savedEmail && savedPassword) {
+    emailUser.value = savedEmail;
+    password.value = savedPassword;
+  }
 });
-
-
-
